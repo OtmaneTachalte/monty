6200282de6de0504d0b2e227e2adafaa34295775
OtmaneTachalte <otmanetachalte@gmail.com>
2023-12-14 11:22:37 -0800

    final1

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *mnop- nothing
+  *@head: stack head
+  *@counter: line_number
+  *Return: no return
+ */
+void mnop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
